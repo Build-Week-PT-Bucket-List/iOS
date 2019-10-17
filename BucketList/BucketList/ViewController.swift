0034b0e97b3628d526e037104ae1a55c45144180
Stephanie <sbowl001@fiu.edu>
2019-10-17 11:27:03 +0800

    storyboard layout, group collab

---
+++ BucketList/BucketList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BucketList
+//
+//  Created by Stephanie Bowles on 10/17/19.
+//  Copyright © 2019 Stephanie Bowles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
