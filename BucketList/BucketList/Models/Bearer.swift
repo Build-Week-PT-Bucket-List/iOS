1de9c6f278efa37341a20e64dbd676c39b6ef2fe
Stephanie <sbowl001@fiu.edu>
2019-10-18 11:00:21 +0800

    models init

---
+++ BucketList/BucketList/Models/Bearer.swift
@@ -0,0 +1,9 @@
+//
+//  Bearer.swift
+//  BucketList
+//
+//  Created by Stephanie Bowles on 10/18/19.
+//  Copyright © 2019 Stephanie Bowles. All rights reserved.
+//
+
+import Foundation
---
+++ BucketList/BucketList/Models/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  BucketList
+//
+//  Created by Stephanie Bowles on 10/18/19.
+//  Copyright © 2019 Stephanie Bowles. All rights reserved.
+//
+
+import Foundation
