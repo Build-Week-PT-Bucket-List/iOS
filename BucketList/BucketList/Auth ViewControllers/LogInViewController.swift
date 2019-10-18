1981cfec4699274abadf67c7c3c7a3a5b9af692c
Stephanie <sbowl001@fiu.edu>
2019-10-18 10:54:34 +0800

    layout setup

---
+++ BucketList/BucketList/Auth ViewControllers/LogInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LogInViewController.swift
+//  BucketList
+//
+//  Created by Stephanie Bowles on 10/18/19.
+//  Copyright © 2019 Stephanie Bowles. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
